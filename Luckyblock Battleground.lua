6c65fc8f56b91e8d2e4aeaa61ffa68b708f00725
SpxzyFire <eren.bouf@gmail.com>
2025-02-04 07:17:22 -0500

    Update and rename Luckyblock Battleground.md to Luckyblock Battleground.lua

--- Luckyblock Battleground.lua
+++ Luckyblock Battleground.lua
@@ -1,6 +1,3 @@
-# Give block Feature
-
-```lua
 game:GetService("ReplicatedStorage"):WaitForChild("SpawnGalaxyBlock"):FireServer() -- change to the other ones if you want
 --[[ if you want, you can replace the "SpawnGalaxyBlock" to the following ones!
 SpawnSuperBlock
