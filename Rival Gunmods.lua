c92e52fd9a5b1d1662dc4258c36680749e421e55
SpxzyFire <eren.bouf@gmail.com>
2025-02-04 07:17:40 -0500

    Update and rename Rival.md to Rival Gunmods.lua

--- Rival Gunmods.lua
+++ Rival Gunmods.lua
@@ -1,5 +1,3 @@
-# Gunmods
-```lua
 local function toggleTableAttribute(attribute, value)
     for _, gcVal in pairs(getgc(true)) do
         if type(gcVal) == "table" and rawget(gcVal, attribute) then
@@ -11,5 +9,3 @@ end
 toggleTableAttribute("ShootCooldown", 0) -- crazy firerate
 toggleTableAttribute("ShootSpread", 0) -- no spread
 toggleTableAttribute("ShootRecoil", 0) --no recoil
-```
-
